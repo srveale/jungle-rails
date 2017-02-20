b06e4035f7efa3ded4bb10869555e4b71a0e8848
Scott Veale <srveale42@gmail.com>
2017-02-20 18:39:07 +0000

    added user spec ;

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
-
   has_many :reviews
+
+  validates :password, presence: true
+  validates :password_validation, presence: true
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    it "is valid with valid attributes" do
+      # user1 =
+      # expect()
+    end
+    it "is invalid without a password"
+    it "is invalid without password_validation"
+  end
+end
