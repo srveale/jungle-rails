e2033fe0f2a5ec8cf822bbdefdbed3fff7f599a3
Scott Veale <srveale42@gmail.com>
2017-02-20 17:19:01 +0000

    added rspec and test widget

--- db/schema.rb
+++ db/schema.rb
@@ -65,9 +65,6 @@ ActiveRecord::Schema.define(version: 20170216200150) do
     t.datetime "updated_at",  null: false
   end
 
-  add_index "reviews", ["product_id"], name: "index_reviews_on_product_id", using: :btree
-  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id", using: :btree
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
